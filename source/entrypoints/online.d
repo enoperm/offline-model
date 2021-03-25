8889bd5e39acfde1f8cd1fe2059e6c1c0818151c
Csaba Sarkadi <61619504+enoperm@users.noreply.github.com>
2021-03-25 15:21:34 +0100

    online-model: implement simulation loop

--- source/entrypoints/online.d
+++ source/entrypoints/online.d
@@ -22,6 +22,8 @@ void entrypoint(ModelContext context) {
         return a;
     }();
 
+    ulong[] countsByRank = new ulong[](context.rankDistribution.length);
+
     SimState[algos.length] simStates =
         algos.map!(_ => SimState(context.queueCount)).array;
 
@@ -32,7 +34,48 @@ void entrypoint(ModelContext context) {
                 .map!(i => Queue(i, i + 1))
         ).join;
 
-    static foreach(i, alg; algos) {{
-        writeln("alg found: ", algoNames[i]);
-    }}
+    // TODO/IMPROVE?: current output contains a lot of redundant information, but it is easy to process
+    void emitState(R)(R output, ulong t, ulong selector) {
+        import asdf: serializeToJson;
+        static struct ReportedState {
+            ulong time;
+            string algorithm;
+            SimState state;
+            Queue[] queues;
+            ulong[] countsByRank;
+        }
+        ReportedState s = {
+            time: t,
+            algorithm: algoNames[selector],
+            state: simStates[selector],
+            queues: queues[selector],
+            countsByRank: countsByRank,
+        };
+        output.put(s.serializeToJson);
+    }
+
+    auto incoming =
+        packets(context.rankDistribution)
+        .take(context.packetCount);
+
+    {
+        auto output = stdout.lockingTextWriter;
+        static foreach(i, alg; algos) emitState(output, 0, i);
+        output.put('\n');
+    }
+
+    foreach(time, packet; incoming.enumerate(1)) {
+        countsByRank[packet] += 1;
+
+        static foreach(i, alg; algos) {{
+            queues[i] = alg(queues[i], countsByRank, packet);
+            simStates[i] = simStates[i].receivePacket(queues[i], packet);
+        }}
+
+        auto output = stdout.lockingTextWriter;
+        static foreach(i, alg; algos) {{
+            emitState(output, time, i);
+        }}
+        output.put('\n');
+    }
 }
